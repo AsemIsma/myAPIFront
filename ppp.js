5c31d827295c01c80891e33a233aab70d8fe093e
Asem <ismailovaasem363@gmail.com>
2025-04-30 01:58:13 +0500

    choosen ones

--- ppp.js
+++ ppp.js
@@ -12,7 +12,7 @@ async function fetchData() {
 
     const data = await response.json();
 
-    console.log(data)
+    console.log(data);
 
     for (let i = 0; i < data.length; i++) {
       document.querySelector(".all-dishes").innerHTML += `
@@ -23,11 +23,22 @@ async function fetchData() {
       <p>${data[i].dishIngredients.map(el => el.join(' ')).join('<br>')}</p>
       <p class="title">Preparation steps:</p>
       <p>-${data[i].dishPrepSteps.join('<br><br>-')}</p>
+      <a class="src" href="${data[i].source}">Source</a>
       <p>author: ${data[i].author}</p>
       </div>
       `;
       dishId.push("dish" + i);
     }
+
+    for (let i = 0; i < data.length; i++) {
+      document.querySelector(`#dish${i}`).addEventListener("click", () => {
+        const target = document.querySelector(`.choosen${i + 1} h2`);
+          target.innerHTML = data[i].dishName;
+          document.querySelector(`.choosen${i + 1} img`).src = data[i].dishImgSrc;
+          console.log("something happen")
+      });
+    }
+
     console.log(dishId)
   }
   catch (error) {
